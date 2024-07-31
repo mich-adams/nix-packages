a7da6b7986ea308e009dc1b0dbc1b6c2800cd448
Michael Adams <me@michdavidadams.com>
2024-07-31 17:52:37 -0400

    moving pkgs from nix-config

--- default.nix
+++ default.nix
@@ -14,7 +14,12 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  #example-package = pkgs.callPackage ./pkgs/example-package { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
+  dosage = pkgs.callPackage ./pkgs/dosage { };
+  exercise-timer = pkgs.callPackage ./pkgs/exercise-timer { };
+  jogger = pkgs.callPackage ./pkgs/jogger { };
+  my-gnu-health = pkgs.callPackage ./pkgs/my-gnu-health { };
+  upscaler = pkgs.callPackage ./pkgs/upscaler { };
 }
